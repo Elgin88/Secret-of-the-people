9e855b42378675a7aa13d0561575dc7ed3601082
Elgin88 <111440347+Elgin88@users.noreply.github.com>
2023-11-22 16:47:43 +0800

    1

--- Assets/Scrips/GameController/GameControllerRequireComponents.cs
+++ Assets/Scrips/GameController/GameControllerRequireComponents.cs
@@ -1,8 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameControllerRequireComponents : MonoBehaviour
 {
-
+    private void Start()
+    {
+        Debug.Log("Выбор платформы и сделать управление джойстиком");
+    }
 }
