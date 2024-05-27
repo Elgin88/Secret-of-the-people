10479ab15f847ab5485ebe1b19ba33f78b2dcf49
Elgin88 <111440347+Elgin88@users.noreply.github.com>
2024-05-27 17:12:41 +0800

    AddCameraFolower

--- Assets/Scrips/PlayerComponents/PlayerCameraFollower.cs
+++ Assets/Scrips/PlayerComponents/PlayerCameraFollower.cs
@@ -4,5 +4,25 @@ namespace Scripts.PlayerComponents
 {
     public class PlayerCameraFollower : MonoBehaviour
     {
+        private float _diagonalOffset = 12;
+        private float _verticalOffset = 0;
+        private float _angleInRadian = 45 * Mathf.PI / 180;
+        private float _horizontal;
+        private float _vertical;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+            _horizontal = Mathf.Cos(_angleInRadian) * _diagonalOffset;
+            _vertical = Mathf.Sin(_angleInRadian) * _diagonalOffset * -1;
+        }
+
+        private void LateUpdate()
+        {
+            _camera.transform.position = new Vector3(transform.position.x, transform.position.y + _horizontal + _verticalOffset, transform.position.z + _vertical);
+
+            _camera.transform.rotation.SetLookRotation(transform.position);
+        }
     }
 }
