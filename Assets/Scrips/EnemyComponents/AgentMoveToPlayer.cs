b4b608465138c725eaed1201659e135ea6ea9091
Elgin88 <111440347+Elgin88@users.noreply.github.com>
2024-06-11 15:56:41 +0800

    eaa

--- Assets/Scrips/EnemyComponents/AgentMoveToPlayer.cs
+++ Assets/Scrips/EnemyComponents/AgentMoveToPlayer.cs
@@ -10,24 +10,50 @@ namespace Scripts.EnemyComponents
     public class AgentMoveToPlayer : MonoBehaviour
     {
         private const float _currentSpeed = 2;
-        private const float _minDistance = 1.5f;
+        private const float _minDistance = 2f;
         private const float _baseSpeed = 0.8f;
         private EnemyAnimator _enemyAnimator;
         private NavMeshAgent _navMeshAgent;
-        private GameObject _player = null;
-        private IGameFactory _gameFactory = null;
+        private GameObject _player;
+        private IGameFactory _gameFactory;
 
         public float NormalizeSpeed => _currentSpeed / _baseSpeed;
 
-        private void Awake()
+        public void AgentOn()
         {
-            _enemyAnimator = GetComponent<EnemyAnimator>();
-            _navMeshAgent = GetComponent<NavMeshAgent>();
-            _navMeshAgent.speed = _currentSpeed;
-            DisableAgentMover();
+            enabled = true;
+            _navMeshAgent.isStopped = false;
         }
 
-        private void DisableAgentMover() => enabled = false;
+        public void AgentOff()
+        {
+            enabled = false;
+            StopMove();
+        }
+
+        private void Awake()
+        {
+            GetComponnents();
+            DisableAgentMover();
+            SetSpeed();
+        }
+
+        private void Update()
+        {
+            if (!IsEnemyOnNavMesh() || !IsPlayerInitialized())
+            {
+                return;
+            }
+
+            if (IsMinDistance())
+            {
+                StartMove();
+            }
+            else
+            {
+                StopMove();
+            }
+        }
 
         private void Start()
         {
@@ -46,36 +72,34 @@ namespace Scripts.EnemyComponents
             }
         }
 
-        private void Update()
+        private void GetComponnents()
         {
-            if (!IsOnNavMesh() || !IsInitializePlayer())
-            {
-                return;
-            }
-
-            if (CheckMinDistance())
-            {
-                Move();
-                _enemyAnimator.PlayMove();
-            }
-            else
-            {
-                _enemyAnimator.StopPlayMove();
-            }
+            _enemyAnimator = GetComponent<EnemyAnimator>();
+            _navMeshAgent = GetComponent<NavMeshAgent>();
         }
 
-        private void Move()
+        private void StartMove()
         {
             _navMeshAgent.destination = _player.transform.position;
+            _navMeshAgent.isStopped = false;
+            _enemyAnimator.PlayMove();
         }
 
-        private bool CheckMinDistance() => Vector3.Distance(transform.position, _player.transform.position) > _minDistance;
-        private bool IsInitializePlayer() => _player != null;
+        private void StopMove()
+        {
+            _navMeshAgent.isStopped = true;
+            _enemyAnimator.StopPlayMove();
+        }
+
+        private bool IsMinDistance() => Vector3.Distance(transform.position, _player.transform.position) > _minDistance;
+        private bool IsPlayerInitialized() => _player != null;
         private void SetGameFactory() => _gameFactory = AllServices.Container.Get<IGameFactory>();
         private void SetPlayerAfterCreate() => _gameFactory.PlayerLoaded += SetPlayer;
         private bool IsPlayerCreate() => _gameFactory.Player != null;
         private bool IsGameFactoryInittialized() => _gameFactory != null;
         private void SetPlayer() => _player = _gameFactory.Player;
-        private bool IsOnNavMesh() => _navMeshAgent.isOnNavMesh;
+        private bool IsEnemyOnNavMesh() => _navMeshAgent.isOnNavMesh;
+        private void DisableAgentMover() => enabled = false;
+        private void SetSpeed() => _navMeshAgent.speed = _currentSpeed;
     }
 }
--- Assets/Scrips/EnemyComponents/EnemyRequireComponents.cs
+++ Assets/Scrips/EnemyComponents/EnemyRequireComponents.cs
@@ -6,6 +6,7 @@ namespace Scripts.EnemyComponents
     [RequireComponent(typeof(Agro))]
     [RequireComponent(typeof(EnemyAnimator))]
     [RequireComponent(typeof(EnemyRequireComponents))]
+    [RequireComponent(typeof(Attack))]
 
     public class EnemyRequireComponents : MonoBehaviour
     {
