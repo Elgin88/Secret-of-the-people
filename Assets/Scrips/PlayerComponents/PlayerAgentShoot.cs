48710b6df651312b2061a0a5385dfd1bf38facee
Elgin88 <111440347+Elgin88@users.noreply.github.com>
2024-08-04 16:27:34 +0800

    1

---
+++ Assets/Scrips/PlayerComponents/PlayerAgentShoot.cs
@@ -0,0 +1,51 @@
+﻿using Scripts.Weapons;
+using UnityEngine;
+
+namespace Scripts.PlayerComponents
+{
+    public class PlayerAgentShoot : MonoBehaviour
+    {
+        [SerializeField] private PlayerChooserWeapon _playerChooserWeapon;
+        [SerializeField] private Transform _shootPoint;
+
+        private float _cooldawn;
+        private float _currentCooldawn;
+        private bool _isAttack = false;
+
+        public Transform ShootPoint => _shootPoint;
+
+        private void FixedUpdate()
+        {
+            Debug.Log("Сделать GunStaticData");
+
+            SetCooldawn();
+            UpdateColdawn();
+            ResetCooldawn();
+
+            if (!_isAttack)
+            {
+                SetIsAttack();
+                Shoot();
+            }
+        }
+
+        private void SetCooldawn() => _cooldawn = _playerChooserWeapon.CurrentWeapon.GetComponent<IWeapon>().DelayBetweenShoots;
+
+        private void ResetCooldawn()
+        {
+            if (_currentCooldawn < 0)
+            {
+                ResetIsAttack();
+                _currentCooldawn = _cooldawn;
+            }
+        }
+
+        private void UpdateColdawn() => _currentCooldawn -= Time.deltaTime;
+
+        private void SetIsAttack() => _isAttack = true;
+        
+        private void ResetIsAttack() => _isAttack = false;
+
+        private void Shoot() => _playerChooserWeapon.ICurrentWeapon.Shoot();
+    }
+}
