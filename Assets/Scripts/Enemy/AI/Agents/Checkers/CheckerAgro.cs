47259af57a151c80d507830a9d68c4990836a521
Elgin88 <111440347+Elgin88@users.noreply.github.com>
2024-09-20 22:20:53 +0800

    1

--- Assets/Scripts/Enemy/AI/Agents/Checkers/CheckerAgro.cs
+++ Assets/Scripts/Enemy/AI/Agents/Checkers/CheckerAgro.cs
@@ -1,11 +1,18 @@
-﻿using Infrastructure.Services.Factory;
+﻿using Agava.YandexGames;
+using Infrastructure.Services.Factory;
+using StaticData;
 using UnityEngine;
 
 namespace Enemy.AI.Agents.Checkers
 {
     public class CheckerAgro : MonoBehaviour
     {
+        [SerializeField] private  MonsterStaticData _staticData;
+        
+        private Transform _player => _gameFactory.Player.transform;
         private IGameFactory _gameFactory;
+        private float _delta = 1;
+
         public bool IsAgro { get; private set; }
 
         public void Construct(IGameFactory gameFactory)
@@ -15,7 +22,20 @@ namespace Enemy.AI.Agents.Checkers
 
         private void FixedUpdate()
         {
+            if (IsNotInAgroRange())
+            {
+                IsAgro = false;
+            }
+            else if (IsInAgroRange())
+            {
+                IsAgro = true;
+            }
             
+            Debug.Log(IsAgro);
         }
+
+        private bool IsNotInAgroRange() => Vector3.Distance(transform.position, _player.position) > _staticData.AgroRange + _delta;
+
+        private bool IsInAgroRange() => Vector3.Distance(transform.position, _player.position) < _staticData.AgroRange;
     }
 }
